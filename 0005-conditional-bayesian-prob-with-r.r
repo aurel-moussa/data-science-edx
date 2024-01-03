2642aecbb772d6033f47da2ee9104cd673fe6ca3
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2024-01-03 16:46:58 +0100

    Update 0005-conditional-bayesian-prob-with-r.r

--- 0005-conditional-bayesian-prob-with-r.r
+++ 0005-conditional-bayesian-prob-with-r.r
@@ -1,1 +1,15 @@
+#hypothetical population of 1 million individuals with the following conditional probabilities
+#a disease test is positive 85% of the time when tested on a patient with the disease (high sensitivity)
+#test is negative 90% of the time when tested on a healthy patient (high specificity)
+#disease is prevalent in 2% of the population
 
+#Bayes theorem
+#P(A given B) = P(B given A)*(P of A) / (P of B)
+#If B is a binary value (like a test result being positive or NOT), we have a special case that P(B) is equal to P(B given NOT A) * P(not A) + P(B given A) * P(A)
+
+# set.seed(1) # if using R 3.5 or earlier
+set.seed(1, sample.kind = "Rounding") # if using R 3.6 or later
+disease <- sample(c(0,1), size=1e6, replace=TRUE, prob=c(0.98,0.02))
+test <- rep(NA, 1e6)
+test[disease==0] <- sample(c(0,1), size=sum(disease==0), replace=TRUE, prob=c(0.90,0.10))
+test[disease==1] <- sample(c(0,1), size=sum(disease==1), replace=TRUE, prob=c(0.15, 0.85))
