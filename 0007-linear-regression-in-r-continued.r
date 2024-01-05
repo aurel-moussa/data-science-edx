52e23e016200a591374c06067ba817e0d065b2a0
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2024-01-05 11:48:24 +0100

    Update 0007-linear-regression-in-r-continued.r

--- 0007-linear-regression-in-r-continued.r
+++ 0007-linear-regression-in-r-continued.r
@@ -7,3 +7,9 @@ n <- 100
 Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
 dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
       data.frame() %>% setNames(c("x", "y"))
+
+#set the seed to 1 again (make sure to use sample.kind="Rounding" if your R is version 3.6 or later). 
+#Then, within a replicate() loop, (1) partition the dataset into test and training sets with p = 0.5 
+#and using dat$y to generate your indices, (2) train a linear model predicting y from x, 
+#(3) generate predictions on the test set, and (4) calculate the RMSE of that model.
+#Then, report the mean and standard deviation (SD) of the RMSEs from all 100 models.
