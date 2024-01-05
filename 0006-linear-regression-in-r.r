bd8b91895d98f99dc70492e4aec817283e7f48c2
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2024-01-05 10:16:14 +0100

    Update 0006-linear-regression-in-r.r

--- 0006-linear-regression-in-r.r
+++ 0006-linear-regression-in-r.r
@@ -17,17 +17,22 @@ test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
 #p the percentage to be included in the partition
 #list FALSE to make a matrix instead of a list, this is required so that later I can index using test_index[1], test_index[2] and so on
 
-train_set <- galton_heights %>% slice(-test_index)
-test_set <- galton_heights %>% slice(test_index)
+train_set <- galton_heights %>% slice(-test_index) #slice the galton_heights set, and drop all the indexes as defined in test-index
+test_set <- galton_heights %>% slice(test_index)  #slice the galton_heights set, and keep all the indexes as defined in test-index
 
+#the simplest model: the average
 avg <- mean(train_set$son)
 avg
 
+#mean square error loss function
 mean((avg - test_set$son)^2)
 
 # fit linear regression model
-fit <- lm(son ~ father, data = train_set)
+fit <- lm(son ~ father, data = train_set) #lm fits linear models, including multivariate ones, first parameter being the symbolic description of the model to be fitted, here: describe son via father
 fit$coef
 
+#predicting y based on the model
 y_hat <- fit$coef[1] + fit$coef[2]*test_set$father
+
+#mean square error loss function
 mean((y_hat - test_set$son)^2)
