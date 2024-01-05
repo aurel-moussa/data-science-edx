05782647b2295f84b27e5f3a721758badd86733f
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2024-01-05 16:00:17 +0100

    Update 0007-linear-regression-in-r-continued.r

--- 0007-linear-regression-in-r-continued.r
+++ 0007-linear-regression-in-r-continued.r
@@ -60,7 +60,7 @@ create_datasets <- function(input_size_of_dataframe) {
 
 dataset_creation_partition_and_model <- function(input_size_of_dataframe) {
         created_dataframe <- create_datasets(input_size_of_dataframe)
-        output_root_mean_square_error <- partition_and_model_function(created_dataframe)
+        output_root_mean_square_error <- replicate(100, partition_and_model_function(created_dataframe))
         mean_output_RMSR <- mean(output_root_mean_square_error)
         sd_output_RMSR <- sd(output_root_mean_square_error)
         print(paste('For size of ', input_size_of_dataframe, 'the mean of the RMSE is ', mean_output_RMSR, 'and the standard deviation ', sd_output_RMSR ))
@@ -68,4 +68,4 @@ dataset_creation_partition_and_model <- function(input_size_of_dataframe) {
 
 set.seed(1, sample.kind="Rounding") # if using R 3.6 or later2, just to ensure reproducibility
 different_dataset_sizes <- c(100, 500, 1000, 5000, 10000)
-sapply(different_dataset_sizes, dataset_creation_partition_and_model(different_dataset_sizes))
+sapply(different_dataset_sizes, dataset_creation_partition_and_model)
