8f6a647199c52018b569fd449a736777bd18a076
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2024-01-05 17:02:28 +0100

    Update 0008-linear-regression-in-r-continued-ii-multiple-predictors.r

--- 0008-linear-regression-in-r-continued-ii-multiple-predictors.r
+++ 0008-linear-regression-in-r-continued-ii-multiple-predictors.r
@@ -1,1 +1,10 @@
+#Now let's work with multiple x's predicting the y
+      
+#Creating a normally distributed dataset
+# set.seed(1) # if using R 3.5 or earlier
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+	data.frame() %>% setNames(c("y", "x_1", "x_2"))
 
+    
