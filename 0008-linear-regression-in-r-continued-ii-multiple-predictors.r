f8fbf59c693679ca55a809e2d8acdc76b2ef4d06
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2024-01-08 08:51:52 +0100

    Update 0008-linear-regression-in-r-continued-ii-multiple-predictors.r

--- 0008-linear-regression-in-r-continued-ii-multiple-predictors.r
+++ 0008-linear-regression-in-r-continued-ii-multiple-predictors.r
@@ -10,3 +10,16 @@ dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
 #y is correlated with both x_1 and x_2 but the two predictors are independent of each other, as seen by cor(dat).
 #Set the seed to 1, then use the caret package to partition into test and training sets with p = 0.5. 
 #Compare the RMSE when using just x_1, just x_2 and both x_1 and x_2. Train a single linear model for each.    
+
+function(input_dataset) {test_index <- createDataPartition(input_dataset$y, times = 1, p = 0.5, list = FALSE) #important! It is actually the y that needs partioning!
+train_set <- input_dataset %>% slice(-test_index) #slice the input_dataset set, and drop all the indexes as defined in test-index
+test_set <- input_dataset %>% slice(test_index)  #slice the input_dataset set, and keep all the indexes as defined in test-index
+fit <- lm(y ~ x, data = train_set) #lm fits linear models, including multivariate ones, first parameter being the symbolic description of the model to be fitted, here: describe y via x
+print(fit$coef)
+y_hat <- predict(fit, test_set)
+root_mean_square_deviation <- mean((y_hat - test_set$y)^2)
+print(root_mean_square_deviation)
+output_square_deviation <- append(output_square_deviation, root_mean_square_deviation)                                                     
+}
+
+#we can make the above more multi-purpose, by allowing the user to specificy which x variables to use for the fitting
