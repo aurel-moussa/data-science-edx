37e30a6dd6fcdac03ec73bb94f30ed6ad2b468e8
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2024-01-08 08:41:28 +0100

    Update 0008-linear-regression-in-r-continued-ii-multiple-predictors.r

--- 0008-linear-regression-in-r-continued-ii-multiple-predictors.r
+++ 0008-linear-regression-in-r-continued-ii-multiple-predictors.r
@@ -7,4 +7,6 @@ Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
 dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
 	data.frame() %>% setNames(c("y", "x_1", "x_2"))
 
-    
+#y is correlated with both x_1 and x_2 but the two predictors are independent of each other, as seen by cor(dat).
+#Set the seed to 1, then use the caret package to partition into test and training sets with p = 0.5. 
+#Compare the RMSE when using just x_1, just x_2 and both x_1 and x_2. Train a single linear model for each.    
